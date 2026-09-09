adc5000653341709b8ce125f7c70636ab0836447
Shivam Singh <shivam.singh97989218@gmail.com>
2026-09-09 18:21:00 +0530

    solved array problem

---
+++ arrays/BestTimeToBuyAndSellStockIII.java
@@ -0,0 +1,35 @@
+package arrays;
+
+import java.util.Arrays;
+
+// l:123
+public class BestTimeToBuyAndSellStockIII {
+    public static void main(String[] args) {
+        int[] prices = new int[]{1,2,3,4,5};
+        System.out.println(maxProfit(prices));
+    }
+    static int maxProfit(int[] prices) {
+        int n = prices.length;
+
+        int[] suffixProfit = new int[n];
+
+        int maxPrice = prices[n - 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            maxPrice = Math.max(maxPrice, prices[i]);
+            suffixProfit[i] = maxPrice - prices[i];
+        }
+
+        int minPrice = prices[0];
+        int prefixProfit = 0;
+        int answer = suffixProfit[0];
+
+        for (int i = 1; i < n; i++) {
+            prefixProfit = Math.max(prefixProfit, prices[i] - minPrice);
+            minPrice = Math.min(minPrice, prices[i]);
+            answer = Math.max(answer, prefixProfit + suffixProfit[i]);
+        }
+
+        return answer; // i have not solve this problem by my own i will solve it later
+    }
+}
